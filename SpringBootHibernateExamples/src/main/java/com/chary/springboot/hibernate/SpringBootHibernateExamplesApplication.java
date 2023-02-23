3553c55469766092c3f88a75b0565a3f53726254
schar <schar@JANGACHARY>
2023-02-23 12:38:26 +0530

    first commit

---
+++ SpringBootHibernateExamples/src/main/java/com/chary/springboot/hibernate/SpringBootHibernateExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.chary.springboot.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateExamplesApplication.class, args);
+	}
+
+}
---
+++ SpringBootHibernateExamples/src/test/java/com/chary/springboot/hibernate/SpringBootHibernateExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chary.springboot.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
